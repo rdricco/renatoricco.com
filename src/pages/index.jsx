57866d47b44d6b904e4d27d061a7f37f6fadfbeb
renato ricco <renato.mur@gmail.com>
2019-11-16 00:47:31 -0300

    index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import Layout from '../layout';
 import About from '../components/About/About';
 import config from '../../data/SiteConfig';
 import styled from 'styled-components';
-import { Flex, Box, Heading, Text, Button, Image, Card } from 'rebass';
+import { Flex, Box, Heading, Text, Link } from 'rebass';
 
 class IndexPage extends Component {
 	render() {
@@ -12,11 +12,36 @@ class IndexPage extends Component {
 			<Layout>
 				<div className='about-container'>
 					<Helmet title={`Home | ${config.siteTitle}`} />
-					<Heading as='h1'>Hi, my name is renato ricco.</Heading>
-					<Text as='h3'> I'm a Creative Coder.</Text>
-					<Text>
-						I was born in São Paulo, where I currently live. Do you have a project we can work together?
-						Nice, feel free to contact me.
+					<Heading
+						as='h1'
+						fontSize={[
+							5,
+							6,
+							7
+						]}
+						color='primary'>
+						Hi, my name is renato ricco.
+					</Heading>
+					<Text
+						as='h3'
+						fontSize={[
+							4,
+							5,
+							6
+						]}
+						color='primary'>
+						I'm a Creative Coder.
+					</Text>
+					<Text
+						fontSize={[
+							3,
+							4,
+							5
+						]}
+						color='primary'>
+						I was born in São Paulo, where I currently live. <br />
+						Do you have a project we can work together? Nice, feel free to
+						<Link href='mailto:contact@renatoricco.com'>contact me.</Link>
 					</Text>
 				</div>
 			</Layout>
